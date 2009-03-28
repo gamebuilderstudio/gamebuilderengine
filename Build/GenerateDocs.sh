3ed0e21617914f611ba237eb47b7d18c81dc5502
HanClinto <HanClinto@6b8e678a-0755-11de-b5bf-434079a3519f>
2009-03-28 03:45:03 +0000

    Added a shell script to generate docs on OS/X with Flex Builder
    git-svn-id: https://pushbuttonengine.googlecode.com/svn/trunk@22 6b8e678a-0755-11de-b5bf-434079a3519f

---
+++ Build/GenerateDocs.sh
@@ -0,0 +1,3 @@
+pushd ..
+/Applications/Adobe\ Flex\ Builder\ 3/sdks/3.0.0/bin/asdoc -source-path Projects/Engine/PBEngine/Source -doc-sources "Projects/Engine/PBEngine/Source" -source-path Projects/Engine/BoxTwoDPhysics/Source -doc-sources "Projects/Engine/BoxTwoDPhysics/Source" -source-path Projects/Engine/MPThreeSound/Source -doc-sources "Projects/Engine/MPThreeSound/Source" -source-path Projects/Engine/TwoDRenderer/Source -doc-sources "Projects/Engine/TwoDRenderer/Source" -output "APIDocs" -main-title "PushButton Engine API" -window-title "PushButton Engine API" -external-library-path "Libraries"
+popd
