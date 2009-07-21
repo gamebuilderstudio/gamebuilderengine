df4dbbcb442db34a1bfbcaf4efb5fb958b362fff
ben.garney <ben.garney@6b8e678a-0755-11de-b5bf-434079a3519f>
2009-07-21 00:53:46 +0000

    - Added fluint as SVN external. - Added new Ant build script to produce docs and SWC.
    git-svn-id: https://pushbuttonengine.googlecode.com/svn/trunk@232 6b8e678a-0755-11de-b5bf-434079a3519f

--- build/GenerateDocs.bat
+++
@@ -1,7 +0,0 @@
-pushd ..
-"c:\Program Files\adobe\Flex Builder 3 Plug-in\sdks\3.2.0\bin\asdoc" ^
-   -source-path Projects/Engine/PBEngine/Source -doc-sources "Projects/Engine/PBEngine/Source" ^
-   -source-path Projects/Engine/BoxTwoDPhysics/Source -doc-sources "Projects/Engine/BoxTwoDPhysics/Source" ^
-   -source-path Projects/Engine/TwoDRenderer/Source -doc-sources "Projects/Engine/TwoDRenderer/Source" ^
-   -output "APIDocs" -main-title "PushButton Engine API" -window-title "PushButton Engine API" -external-library-path "Libraries"
-popd
